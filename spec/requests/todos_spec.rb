a76ada08bc1e3428497d564d3a600385f577ec3a
Justin <beanjustinr@gmail.com>
2017-04-10 21:53:29 -0400

    controllers and routes added for todos and items

--- spec/requests/todos_spec.rb
+++ spec/requests/todos_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-Rspec.describe 'Todos API', type: :request do
+RSpec.describe 'Todos API', type: :request do
   let!(:todos) { create_list(:todo, 10) }
   let(:todo_id) { todos.first.id }
   describe 'GET /todos' do
